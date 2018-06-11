4c3b1c4b5c09ec0378960b3a998cb164aa87931e
Mathias <Fysikeren@users.noreply.github.com>
2018-06-11 18:42:46 +0200

    The Julia implementation for a one-layer coil is done

--- coilAndField.jl
+++ coilAndField.jl
@@ -3,11 +3,11 @@ include("meshgrid.jl")
 include("elementOfLength.jl")
 include("coordinateOfCoil.jl")
 
-N = 2 # Number of turns in coil
+N = 125 # Number of turns in coil
 l = 25e-3 # Length of coil, m
 a0 = 7.25e-3 # Inner radius of coil, m
 da = 0.2e-3 # The thickness of one layer of turns. This is the outer diameter of the magnet wire, which includes the enamel
-n = 1e2 # Number of points in the model of the coil
+n = Int(1e4) # Number of points in the model of the coil
 
 # Calculate the coordinates for a simple one-layer coil:
 a = a0 + da/2
@@ -15,18 +15,41 @@ x,y,z = coordinateOfCoil(N,l,a,n)
 dx,dy,dz = elementOfLength(N,l,a,n)
 
 # Calculate coordinates of points to test the magnetic field vectors
-nGrid = 5
+nGrid = 75
 xs = linspace(-1.4*a,1.4*a,nGrid)
 zs = linspace(-0.7*l,0.7*l,2*nGrid) # The length of the coil is roughly twice its diameter, so we use twice as many points along the z direction
 X,Z = meshgrid(xs,zs)
 Y = zeros(size(X))
 
 Bx = zeros(size(X))
-By = Bx
-Bz = Bx
+By = zeros(size(X))
+Bz = zeros(size(X))
 
 nTestPoints = length(X[:])
 
+tic()
+for i in 1:nTestPoints
+    testPoint = [X[i]; Y[i]; Z[i]]
+    db = zeros(3,n)
+
+    for j in 1:n
+        dl = [dx[j]; dy[j]; dz[j]]
+        r = testPoint - [x[j]; y[j]; z[j]]
+        rNorm = norm(r)
+
+        db[:,j] = cross(dl,r)/(rNorm^3)
+    end
+
+    I = 1e-3
+    db *= I*1e-7
+    B = sum(db,2)
+    Bx[i] = B[1]
+    By[i] = B[2]
+    Bz[i] = B[3]
+end
+toc()
+
 using Plots
 plotly() # Choose the Plotly.jl backend for web interactivity
-surface(X,Z,Bz)
+plot(x,y,z)
+# surface(X,Z,Bz)
