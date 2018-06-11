7a404d5f6291b5a4729725a33dde10d39b5074e9
Mathias <Fysikeren@users.noreply.github.com>
2018-06-11 16:50:10 +0200

    Included the actual code

--- meshgrid.jl
+++ meshgrid.jl
@@ -1,12 +1,12 @@
-include("myMeshgrid.jl")
-
-x = linspace(0,1,3)
-y = x
-
-X,Y = meshgrid(x,y)
-
-println(x)
-println(y)
-
-println(X)
-println(Y)
+# meshgrid
+# Emulates the 2D version of Matlab's meshgrid function
+# Needs expansion to 3D
+function meshgrid(x,y)
+    nx = length(x)
+    ny = length(y)
+  
+    X = repmat(x',ny,1)
+    Y = repmat(y,1,nx)
+  
+    return X,Y
+end
