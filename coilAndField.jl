e70371ee47b44d53420965eff887d719b5588743
Mathias <Fysikeren@users.noreply.github.com>
2018-06-11 17:06:35 +0200

    Add initialization and plotting of the field

--- coilAndField.jl
+++ coilAndField.jl
@@ -14,4 +14,19 @@ a = a0 + da/2
 x,y,z = coordinateOfCoil(N,l,a,n)
 dx,dy,dz = elementOfLength(N,l,a,n)
 
-# Calculate coordinates of points 
+# Calculate coordinates of points to test the magnetic field vectors
+nGrid = 5
+xs = linspace(-1.4*a,1.4*a,nGrid)
+zs = linspace(-0.7*l,0.7*l,2*nGrid) # The length of the coil is roughly twice its diameter, so we use twice as many points along the z direction
+X,Z = meshgrid(xs,zs)
+Y = zeros(size(X))
+
+Bx = zeros(size(X))
+By = Bx
+Bz = Bx
+
+nTestPoints = length(X[:])
+
+using Plots
+plotly() # Choose the Plotly.jl backend for web interactivity
+surface(X,Z,Bz)
