3d6f3ea9dbfd9e453e767b7e1b9bd6eb09c5a101
Mathias <Fysikeren@users.noreply.github.com>
2018-06-11 21:51:08 +0200

    Make contour plots of the three B-field components in Python

--- plotResults.py
+++ plotResults.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+# from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
 # X = np.genfromtxt("Output/X.txt", unpack=True)
 X = np.genfromtxt("Output/X.txt")
 Z = np.genfromtxt("Output/Z.txt")
+Bx = np.genfromtxt("Output/Bx.txt")
+By = np.genfromtxt("Output/By.txt")
 Bz = np.genfromtxt("Output/Bz.txt")
 
-plt.contour(X,Z,Bz)
+plt.clf()
+plt.contourf(X,Z,Bx)
+plt.savefig('Output/Bx.png')
+
+plt.clf()
+plt.contourf(X,Z,By)
+plt.savefig('Output/By.png')
+
+plt.clf()
+plt.contourf(X,Z,Bz)
+plt.savefig('Output/Bz.png')
+
+# Axes3D.plot_surface(X,Z,Bz)
+# plt.show()
