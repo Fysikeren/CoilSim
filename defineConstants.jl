fcb7c0f6c7c67472fb773c7c010bd6ed19639b9e
Mathias <Fysikeren@users.noreply.github.com>
2018-06-12 13:37:28 +0200

    Run a full coil

--- defineConstants.jl
+++ defineConstants.jl
@@ -2,10 +2,10 @@
 # Outputs the variables necessary for calculating the coil.
 # Could be changed into a promt for the user.
 function defineConstants()
-    N = 10 # Number of turns in coil
+    N = 125 # Number of turns in coil
     a0 = 7.25e-3 # Inner radius of coil, m
     Δa = 0.2e-3 # The thickness of one layer of turns. This is the outer diameter of the magnet wire, which includes the enamel
     l = N*Δa # Length of coil, m
-    n = Int(1e2) # Number of points in the model of the coil
+    n = Int(1e4) # Number of points in the model of the coil
     return N,l,a0,Δa,n
 end
