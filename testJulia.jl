f5c9c9c1d2951f687cc0a1b6fd824674fc21b94d
Mathias <Fysikeren@users.noreply.github.com>
2018-06-12 12:03:19 +0200

    Test calculation of element lengths for different numbers of elements

--- testJulia.jl
+++ testJulia.jl
@@ -4,11 +4,20 @@ include("elementOfLength.jl")
 
 N,l,a0,Δa,n = defineConstants()
 
-println("l = ",l*1e3," mm")
-
 a = a0 + Δa/2
-Δx,Δy,Δz = elementOfLength(N,l,a,n)
 
-println(sum(sqrt.(Δx.^2 + Δy.^2 + Δz.^2))," is the sum of the element lengths.")
-println(sqrt((N*2*pi*a)^2 + (l)^2)," is the length of the wire used.")
-println(2*pi*a*N," is N times the coil circumference.")
+wireLength = sqrt((N*2*pi*a)^2 + (l)^2)
+totalCircumference = 2*pi*a*N
+
+println(wireLength," is the length of the wire used.")
+println(totalCircumference," is N times the coil circumference.")
+
+number = 5
+# lengthSums = zeros(number,1)
+
+for n in logspace(0,number,number + 1)
+	Δx,Δy,Δz = elementOfLength(N,l,a,n)
+	lengthSum = sum(sqrt.(Δx.^2 + Δy.^2 + Δz.^2))
+
+	println(lengthSum," is the sum of the element lengths for n = ",n,".")
+end
