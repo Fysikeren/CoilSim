7db408bd3821f9e98f0cea24bc183df422133e29
Mathias <Fysikeren@users.noreply.github.com>
2018-06-12 21:37:57 +0200

    Plot the plane of test points horizontally

--- plotResults.m
+++ plotResults.m
@@ -10,15 +10,30 @@ Bx = importdata('Output/Bx.txt');
 By = importdata('Output/By.txt');
 Bz = importdata('Output/Bz.txt');
 
+% Mathematica uses "Indeterminate" where Matlab would use "NaN", so we replace
+% those:
+!sed -i 's/Indeterminate/NaN/g' Output/XZBzAnalytic.txt
+XZBzAnalytic = importdata('Output/XZBzAnalytic.txt');
+
+n = sqrt(size(XZBzAnalytic,1)/2);
+
+% X = reshape(XZBzAnalytic(:,1),2*n,n);
+% Z = reshape(XZBzAnalytic(:,2),2*n,n);
+BzAnalytic = reshape(XZBzAnalytic(:,3),2*n,n);
+
 whos
 
 setPlotSize(16,9,'centimeter')
 
 clf; hold on
-plot3(coilCoordinates(:,1),coilCoordinates(:,2),coilCoordinates(:,3))
+plot3(coilCoordinates(:,1),coilCoordinates(:,3),coilCoordinates(:,2))
+xlabel('x')
+ylabel('z')
+zlabel('y')
 xlim([min(min(X)) max(max(X))])
-ylim([min(min(X)) max(max(X))])
-zlim([min(coilCoordinates(:,3)) max(coilCoordinates(:,3))])
+ylim([min(min(Z)) max(max(Z))])
+% zlim([min(coilCoordinates(:,3)) max(coilCoordinates(:,3))])
+zlim([min(min(X)) max(max(X))])
 view(30,30)
 box on
 grid on
@@ -27,61 +42,101 @@ print(1,filename,'-dpng','-r300')
 % savefig(1,filename)
 % matlab2tikz([filename '.tikz'],'height','\figureheight','width','\figurewidth','showInfo',false);
 
-quiver3(X,Y,Z,Bx,By,Bz)
+quiver3(X,Z,Y,Bx,Bz,By)
+xlabel('x')
+ylabel('z')
+zlabel('y')
 xlim([min(min(X)) max(max(X))])
-ylim([min(min(X)) max(max(X))])
-zlim([min(coilCoordinates(:,3)) max(coilCoordinates(:,3))])
+ylim([min(min(Z)) max(max(Z))])
+% zlim([min(coilCoordinates(:,3)) max(coilCoordinates(:,3))])
+zlim([min(min(X)) max(max(X))])
 view(30,30)
 box on
 grid on
 filename = 'Output/theField';
 print(1,filename,'-dpng','-r300')
 
-% minimumField = min([min(min(Bx)) min(min(By)) min(min(Bz))]);
-% maximumField = max([max(max(Bx)) max(max(By)) max(max(Bz))]);
-minimumField = -6e-6;
-maximumField = 10e-6;
+minimumField = min([min(min(Bx)) min(min(By)) min(min(Bz)) min(min(BzAnalytic))]);
+maximumField = max([max(max(Bx)) max(max(By)) max(max(Bz)) max(max(BzAnalytic))]);
+% minimumField = -6e-6;
+% maximumField = 10e-6;
 
-clf
+clf; hold on
 mesh(X,Z,Bx)
+xlabel('x')
+ylabel('z')
+zlabel('B_x')
+% plot3(coilCoordinates(:,1),coilCoordinates(:,3),coilCoordinates(:,2))
+xlim([min(min(X)) max(max(X))])
+ylim([min(min(Z)) max(max(Z))])
 zlim([minimumField maximumField])
+caxis([minimumField maximumField])
 view(30,30)
 box on
+grid on
 filename = 'Output/Bx';
 print(1,filename,'-dpng','-r300')
 
-clf
+clf; hold on
 mesh(X,Z,By)
+xlabel('x')
+ylabel('z')
+zlabel('B_y')
+% plot3(coilCoordinates(:,1),coilCoordinates(:,3),coilCoordinates(:,2))
+xlim([min(min(X)) max(max(X))])
+ylim([min(min(Z)) max(max(Z))])
 zlim([minimumField maximumField])
+caxis([minimumField maximumField])
 view(30,30)
 box on
+grid on
 filename = 'Output/By';
 print(1,filename,'-dpng','-r300')
 
-clf
+clf; hold on
 mesh(X,Z,Bz)
+xlabel('x')
+ylabel('z')
+zlabel('B_z')
+% plot3(coilCoordinates(:,1),coilCoordinates(:,3),coilCoordinates(:,2))
+xlim([min(min(X)) max(max(X))])
+ylim([min(min(Z)) max(max(Z))])
 zlim([minimumField maximumField])
+caxis([minimumField maximumField])
 view(30,30)
 box on
+grid on
 filename = 'Output/Bz';
 print(1,filename,'-dpng','-r300')
 
-% clear
-
-XZBzAnalytic = importdata('Output/XZBzAnalytic.txt');
-
-n = sqrt(size(XZBzAnalytic,1)/2);
-
-X = reshape(XZBzAnalytic(:,1),2*n,n);
-Z = reshape(XZBzAnalytic(:,2),2*n,n);
-BzAnalytic = reshape(XZBzAnalytic(:,3),2*n,n);
-
-whos
-
-clf
+clf; hold on
 mesh(X,Z,BzAnalytic)
+xlabel('x')
+ylabel('z')
+zlabel('B_{z,Analytic}')
+% plot3(coilCoordinates(:,1),coilCoordinates(:,3),coilCoordinates(:,2))
+xlim([min(min(X)) max(max(X))])
+ylim([min(min(Z)) max(max(Z))])
 zlim([minimumField maximumField])
+caxis([minimumField maximumField])
 view(30,30)
 box on
+grid on
 filename = 'Output/BzAnalytic';
+print(1,filename,'-dpng','-r300')
+
+clf; hold on
+mesh(X,Z,Bz - BzAnalytic)
+xlabel('x')
+ylabel('z')
+zlabel('B_z - B_{z,Analytic}')
+% plot3(coilCoordinates(:,1),coilCoordinates(:,3),coilCoordinates(:,2))
+xlim([min(min(X)) max(max(X))])
+ylim([min(min(Z)) max(max(Z))])
+% zlim([minimumField maximumField])
+caxis([minimumField maximumField])
+view(30,30)
+box on
+grid on
+filename = 'Output/BzDifference';
 print(1,filename,'-dpng','-r300')
