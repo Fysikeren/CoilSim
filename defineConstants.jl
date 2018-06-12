0b1c48b9c0490b86a0b29967656d7c5404b95b46
Mathias <Fysikeren@users.noreply.github.com>
2018-06-12 10:34:15 +0200

    Make a coil with 10 windings

--- defineConstants.jl
+++ defineConstants.jl
@@ -2,10 +2,11 @@
 # Outputs the variables necessary for calculating the coil.
 # Could be changed into a promt for the user.
 function defineConstants()
-    N = 100 # Number of turns in coil
-    l = 25e-3 # Length of coil, m
+    N = 10 # Number of turns in coil
+    # l = 25e-3 # Length of coil, m
     a0 = 7.25e-3 # Inner radius of coil, m
     Δa = 0.2e-3 # The thickness of one layer of turns. This is the outer diameter of the magnet wire, which includes the enamel
-    n = Int(1e4) # Number of points in the model of the coil
+    l = N*Δa # Length of coil, m
+    n = Int(1e3) # Number of points in the model of the coil
     return N,l,a0,Δa,n
 end
